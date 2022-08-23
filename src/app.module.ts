f1f7518a2dbabc446f1cd32fab006866970a768b
vaughanstech <vaughanstech@gmail.com>
2022-08-22 22:44:27 -0400

    created authentication middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma.service';
 import { AutobotsModule } from './services/autobots/autobots.module';
 import { DecepticonsModule } from './services/decepticons/decepticons.module';
 
 @Module({
-  imports: [AutobotsModule, DecepticonsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    AutobotsModule,
+    DecepticonsModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
