41c38d8a5d0585b198b7ae57c73c90da7c444d88
vaughanstech <vaughanstech@gmail.com>
2022-08-23 21:20:39 -0400

    setup swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Transformers API')
+    .setDescription(
+      'Include all your favorite transformers in your app with this fun API',
+    )
+    .setVersion('beta')
+    .addTag('Transformers')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const customOptions: SwaggerCustomOptions = {
+    customCss: '.swagger-ui .topbar {display: none}',
+  };
+  SwaggerModule.setup('api', app, document, customOptions);
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
